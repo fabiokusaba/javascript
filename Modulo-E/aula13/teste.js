fdfb0ca7d84d0f7d278234011a7306b76de0707b
Fabio Kusaba <fabinhokb44@gmail.com>
2023-06-11 15:19:53 -0300

    Aula estruturas de repetição com teste lógico

---
+++ Modulo-E/aula13/teste.js
@@ -0,0 +1,20 @@
+// Estrutura de repetição while com teste lógico no início
+var c = 1
+while (c <= 6) {
+  console.log(`Passo ${c}`)
+  c++ // c = c + 1
+}
+
+// Estrutura de repetição com teste lógico no fim
+var cont = 1
+do {
+  console.log(`Contei ${cont}`)
+  cont++
+} while(cont <= 6)
+
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
