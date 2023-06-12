6883ef01400fbf7ffd0a9879d21a4c3de3fa449e
Fabio Kusaba <fabinhokb44@gmail.com>
2023-06-12 11:46:52 -0300

    Ajustes finais Ex 16

--- Modulo-E/aula14ex/ex16/script.js
+++ Modulo-E/aula14ex/ex16/script.js
@@ -9,14 +9,14 @@ function contar() {
   var passoInput = document.getElementById('txtpasso')
   var res = document.querySelector('div#res')
 
-  // Checando se os campos estão preenchidos
+  // Checando se os campos estão vazios
   if (inicioInput.value.length == 0 || fimInput.value.length == 0 || passoInput.value.length == 0) {
     res.innerHTML = `Impossível contar!`
     alert('[ERRO] Faltam dados a serem preenchidos!')
   } else {
-    res.innerHTML = `<p>Contando...</p>`
+    res.innerHTML = `Contando: `
 
-    // Capturando os valores
+    // Convertendo valores string para number
     var inicio = Number(inicioInput.value)
     var fim = Number(fimInput.value)
     var passo = Number(passoInput.value)
@@ -30,15 +30,16 @@ function contar() {
     // Checando se o início é menor que o fim - Ordem crescente
     if (inicio < fim) {
       for (var cont = inicio; cont <= fim; cont += passo) {
-        res.innerHTML += `${cont} `
+        res.innerHTML += `${cont} \u{1F449}` // Adicionando emoji - Unicode
       }
     }
 
     // Checando se o início é maior que o fim - Ordem decrescente
     if (inicio > fim) {
       for (var cont = inicio; cont >= fim; cont -= passo) {
-        res.innerHTML += `${cont} `
+        res.innerHTML += `${cont} \u{1F449}` // Adicionando emoji - Unicode
       }
     }
+    res.innerHTML += `\u{1F3C1}` // Adicionando emoji - Unicode
   }
 }
